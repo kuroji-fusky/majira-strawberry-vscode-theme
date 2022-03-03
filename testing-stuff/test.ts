c459c0140847048d81692c24e5f09f5f304af4c2
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-04 04:29:44 +0800

    Added typescript code for testing

---
+++ testing-stuff/test.ts
@@ -0,0 +1,33 @@
+interface user {
+  name: string;
+  species: string;
+  height: number;
+  id: number;
+}
+
+class someone {
+  name: string;
+  species: string;
+  height: number;
+  id: number;
+
+  constructor(name: string, fursona: string, height: number, id: number) {
+    this.name = name;
+    this.species = fursona;
+    this.height = height;
+    this.id = id;
+  }
+}
+
+const furryYouTuber: user = new someone("Majira Strawberry", "Fox", 6.9, 420);
+
+interface findCutie {
+  name: string,
+  fursona: string
+}
+
+function identifyYourselfMF(getDatCutie: findCutie) {
+  console.log(
+    `Hello there ${getDatCutie.name}, you're a ${getDatCutie.fursona} right? You're such a cutie!`
+  );
+}
